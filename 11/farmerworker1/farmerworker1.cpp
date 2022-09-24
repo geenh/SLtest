460bd98bbba9a4317562adb8a322f951f5c7ddbe
gen_hao123 <2900115163@qq.com>
2022-09-24 16:59:31 +0800

    SL

---
+++ 11/farmerworker1/farmerworker1.cpp
@@ -0,0 +1,70 @@
+﻿#include<iostream>
+using namespace std;
+class Farmer
+{
+public:
+	Farmer()
+	{
+		cout<<"Farmer()" << endl;
+	}
+	~Farmer()
+	{
+		cout<<"~Farmer()" << endl;
+	}
+	void Sow()
+	{
+		cout<<"播种" << endl;
+	}
+	
+	void eat()
+	{
+		cout<<"Farmer::eat()" << endl;
+	}
+};
+class Worker
+{
+public:
+	Worker()
+	{
+		cout<<"Worker()" << endl;
+	}
+	~Worker()
+	{
+		cout<<"~Worker()" << endl;
+	}
+	void Work()
+	{
+		cout<<"工作" << endl;
+	}
+	void eat()
+	{
+		cout<<"Worker::eat()" << endl;
+	}
+
+};
+
+class FarmerWorker :public Farmer, public Worker
+{
+public:
+	FarmerWorker()
+	{
+		cout<<"FarmerWorker()" << endl;
+	}
+	~FarmerWorker()
+	{
+		cout<<"~FarmerWorker()" << endl;
+	}
+	void eat()
+	{
+		Worker::eat();
+	}
+};
+int main()
+{
+	FarmerWorker fm;
+	cout << endl;
+	fm.Farmer::eat();
+	fm.eat();
+	cout << endl;
+	return 0;
+}
---
+++ 11/multiDerived/multiDerived.cpp
@@ -0,0 +1,71 @@
+﻿#include<iostream>
+using namespace std;
+
+class Date
+{
+public:
+	Date()
+	{
+		cout<<"Date()" << endl;
+	}
+	~Date()
+	{
+		cout<<"~Date()" << endl;
+	}
+
+};
+class Farmer
+{
+public:
+	Farmer()
+	{
+		cout<<"Farmer()" << endl;
+	}
+	~Farmer()
+	{
+		cout<<"~Farmer()" << endl;
+	}
+	void Sow()
+	{
+		cout<<"播种" << endl;
+	}
+};
+
+class Worker
+{
+public:
+	Worker()
+	{
+		cout<<"Worker()" << endl;
+	}
+	~Worker()
+	{
+		cout<<"~Worker()" << endl;
+	}
+	void Work()
+	{
+		cout<<"工作" << endl;
+	}
+};
+
+class FarmerWorker :public Farmer, public Worker
+{
+public:
+	FarmerWorker()
+	{
+		cout<<"FarmerWorker()" << endl;
+	}
+	~FarmerWorker()
+	{
+		cout<<"~FarmerWorker()" << endl;
+	}
+	Date d1;
+	Date d2;
+};
+int main()
+{
+	FarmerWorker fm;
+	fm.Sow();
+	fm.Work();
+	return 0;
+}
