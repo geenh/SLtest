9773c9291899591301d6aa607dc13b65110a1fba
ike1009 L <2900115163@qq.com>
2022-10-25 20:32:28 +0800

    SL

---
+++ 12/Sample5/Sample5/Sample5.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string>
+using namespace std;
+template<class T1, class T2,int num=10>
+class MyTemplate
+{
+	T1 t1;
+	T2 t2;
+public:
+	MyTemplate(T1 tt1, T2 tt2)
+	{
+		t1 = tt1+num, t2 = tt2+num;
+	}
+	void display()
+	{
+		cout << t1 << " " << t2 << endl;
+	}
+};
+int main()
+{
+	int a = 100;
+	float b = 123.45f;
+	MyTemplate<int, float, 20> ta(a, b);
+	ta.display();
+
+	MyTemplate<int, float> tb(a, b);
+	tb.display();
+}
