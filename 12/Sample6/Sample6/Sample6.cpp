35dbb26132bb9c4a579a99f56da1072cd62522df
ike1009 L <2900115163@qq.com>
2022-10-25 20:38:57 +0800

    SL

---
+++ 12/Sample6/Sample6/Sample6.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string>
+#include<assert.h>
+using namespace std;
+
+template<class T, int b>
+class Array
+{
+	T elem[b];
+public:
+	Array(){}
+	T& operator[](int sub)
+	{
+		assert(sub >= 0 && sub < b);
+		return elem[sub];
+	}
+};
+int main()
+{
+	Array<int, 10> array;
+	array[0]=0;
+	
+
+}
